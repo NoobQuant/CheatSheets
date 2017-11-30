26124916b758caeecb90880ba64b8ecc28d9c003
NoobQuant <ville.v.voutilainen@gmail.com>
2017-11-30 17:31:03 +0200

    Add files via upload

--- SQL_Cheat_Sheet.sql
+++ SQL_Cheat_Sheet.sql
@@ -99,8 +99,9 @@ GROUP BY
 /**************************************/
 
 
+
 -- Fill missing values with UPDATE and INNER JOIN
-/**************************************/
+/*****************OPEN*********************/
 
 
 IF OBJECT_ID('tempdb..#temp_test') IS NOT NULL DROP TABLE #temp_test 
@@ -129,4 +130,61 @@ WHERE dt1.[value] IS NULL AND dt2.[value] IS NOT NULL
 SELECT * FROM #temp_test
 
 
-/**************************************/
+/****************CLOSE*********************/
+
+
+
+
+
+
+-- Avoid nested loops due to calculated columns using OUTER APPLY
+/*****************OPEN*********************/
+
+
+IF OBJECT_ID('tempdb..#temp_test') IS NOT NULL DROP TABLE #temp_test 
+
+SELECT 'A' AS [name], 2007 AS [year] , 4 AS [value] 
+INTO #temp_test 
+
+UNION ALL 
+SELECT 'B', 2007, 6
+UNION ALL 
+SELECT 'C', 2007, 2
+UNION ALL 
+SELECT 'A', 2008, 1
+UNION ALL 
+SELECT 'B', 2008, 3
+UNION ALL 
+SELECT 'C', 2008, 5
+
+-- 1st calculated column is put to OUTER APPLY
+-- 2nd calculated column can be done in SELECT
+SELECT 
+	 [name]
+	,[year]
+	,[value]
+	,B.new_col
+	,B.new_col + 2 AS new_col2
+FROM #temp_test AS A
+OUTER APPLY (SELECT [year] - [value] as new_col ) AS B
+
+/****************CLOSE*********************/
+
+
+
+
+
+-- Info on CROSS APPLY vs OUTER APPLY
+/*****************OPEN********************
+
+CROSS APPLY is SQL Servre specific
+
+CROSS Apply is like an inner join and will only return the results from the parent 
+table where it is getting a result from the function.
+
+OUTER Apply can be compared to a left outer join and shows all the records from the 
+parent table, plus will show a null if no result from the function
+
+****************CLOSE*********************/
+
+
