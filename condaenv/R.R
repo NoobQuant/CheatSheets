5acb30d0d4801ec63c102d76a73d74b0524c699e
ville_home <ville.v.voutilainen@gmail.com>
2021-06-09 22:04:53 +0300

    remove old files

--- condaenv/R.R
+++
@@ -1,9 +0,0 @@
-# Minimalistic .R test file
-# See R.ipynb for more robust tests.
-
-# Load some stuff
-library(tidyverse)
-library(ggplot2)
-
-# Print location of exe
-print(file.path(R.home("bin"), "R"))
